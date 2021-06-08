29f08e979a29693cdca76efeb8e4f293b204788e
Lakshit Yadav <59692570+LakshitYadav@users.noreply.github.com>
2021-06-08 20:17:44 +0530

    Delete Final directory

--- Final/assignment-react-backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const authController = require('../controller/authController');
-const express = require('express');
-
-const authRoutes = express.Router();
-
-authRoutes.post('/login', authController.login);
-
-
-module.exports = authRoutes;
--- Final/assignment-react-backend/utils/database.js
+++
@@ -1,9 +0,0 @@
-
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('assignment-react-backend', 'root', 'lakshit', {
-    dialect: 'mysql', 
-    host: 'localhost'
-});
-
-module.exports = sequelize;
